fd7ba6bbcaa4ecb06e23aea30c2cf7efdc0cce55
toomanybrians <brian@missionpinball.com>
2016-09-24 11:52:45 -0700

    updated tests

--- tests/test_bnd.py
+++ tests/test_bnd.py
@@ -2,15 +2,59 @@ from mpf.core.rgb_color import RGBColor
 from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
 
 
-class TestBnD(MpfMachineTestCase):
+class TestBnd(MpfMachineTestCase):
+
+    def test_machine_boot(self):
+
+        # game starts with drain and trough switches active
+        # let's make sure the counts are right
+
+        self.assertEqual(self.machine.ball_devices.bd_drain.balls, 1)
+        self.assertEqual(self.machine.ball_devices.bd_trough.balls, 3)
+        self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 0)
+
+        # advance 10 secs and make sure everything is still right
+        self.advance_time_and_run(10)
+
+        self.assertEqual(self.machine.ball_devices.bd_drain.balls, 1)
+        self.assertEqual(self.machine.ball_devices.bd_trough.balls, 3)
+        self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 0)
 
     def test_attract_mode(self):
         # make sure attract mode is running
         self.assertTrue(self.machine.modes['attract'].active)
 
-        self.advance_time_and_run()
-
         # make sure the GI comes on
         for led in self.machine.leds.items_tagged('gi'):
             self.assertEqual(RGBColor('66ff18'),
                 led.hw_driver.current_color)
+
+        # make sure the attract mode shows are running
+
+    def test_single_player_game(self):
+        self.hit_and_release_switch('s_start')
+
+        self.advance_time_and_run(4)
+
+        # ball should be sitting in the plunger lane
+
+        self.assertEqual(self.machine.ball_devices.bd_drain.balls, 0)
+        self.assertEqual(self.machine.ball_devices.bd_trough.balls, 2)
+        self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 1)
+
+        # game should be running
+        self.assertIsNotNone(self.machine.game)
+        self.assertEqual(1, self.machine.game.player.ball)
+
+        # make sure ball ejected to plunger, and ball in drain left
+        self.advance_time_and_run(1)
+
+        # playfield expects a ball
+        self.assertEqual(1, self.machine.playfield.available_balls)
+
+        # but its not there yet
+        self.assertEqual(0, self.machine.playfield.balls)
+
+        # after 20 it's still not there
+        self.advance_time_and_run(20)
+        self.assertEqual(1, self.machine.playfield.balls)
