e443e3f7a58485952fe5fa464e159be40ca0a604
toomanybrians <brian@missionpinball.com>
2016-09-24 16:03:57 -0700

    added more tests

--- tests/test_bnd.py
+++ tests/test_bnd.py
@@ -1,5 +1,6 @@
 from mpf.core.rgb_color import RGBColor
 from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
+from mpfmc.tests.FullMpfMachineTestCase import FullMachineTestCase
 
 
 class TestBnd(MpfMachineTestCase):
@@ -22,6 +23,12 @@ class TestBnd(MpfMachineTestCase):
         self.assertEqual(self.machine.ball_devices.bd_trough.balls, 3)
         self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 0)
 
+        self.advance_time_and_run(10)
+
+        self.assertEqual(self.machine.ball_devices.bd_drain.balls, 1)
+        self.assertEqual(self.machine.ball_devices.bd_trough.balls, 3)
+        self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 0)
+
     def test_attract_mode(self):
         # make sure attract mode is running
         self.assertTrue(self.machine.modes['attract'].active)
@@ -33,23 +40,27 @@ class TestBnd(MpfMachineTestCase):
 
         # make sure the attract mode shows are running
 
-    def test_single_player_game(self):
+    def _dump_balls(self):
+        print('-------------')
+        print('drain:    ', self.machine.ball_devices.bd_drain.balls)
+        print('trough:   ', self.machine.ball_devices.bd_trough.balls)
+        print('plunger:  ', self.machine.ball_devices.bd_plunger.balls)
+        print('playfield:', self.machine.ball_devices.playfield.balls)
+
+    def _start_single_player_game(self, secs_since_plunge):
         self.hit_and_release_switch('s_start')
-
-        self.advance_time_and_run(4)
-
-        # ball should be sitting in the plunger lane
-
-        self.assertEqual(self.machine.ball_devices.bd_drain.balls, 0)
-        self.assertEqual(self.machine.ball_devices.bd_trough.balls, 2)
-        self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 1)
 
         # game should be running
         self.assertIsNotNone(self.machine.game)
         self.assertEqual(1, self.machine.game.player.ball)
 
-        # make sure ball ejected to plunger, and ball in drain left
-        self.advance_time_and_run(1)
+        # advance enough time for the balls to eject and stuff
+        self.advance_time_and_run()
+
+        # ball should be sitting in the plunger lane
+        self.assertEqual(self.machine.ball_devices.bd_drain.balls, 0)
+        self.assertEqual(self.machine.ball_devices.bd_trough.balls, 3)
+        self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 1)
 
         # playfield expects a ball
         self.assertEqual(1, self.machine.playfield.available_balls)
@@ -57,6 +68,62 @@ class TestBnd(MpfMachineTestCase):
         # but its not there yet
         self.assertEqual(0, self.machine.playfield.balls)
 
-        # after 20 it's still not there
+        # after 20s it's still not there
         self.advance_time_and_run(20)
+        self.assertEqual(0, self.machine.playfield.balls)
+
+        # player mechanically ejects
+        self.machine.switch_controller.process_switch('s_plungerlane', 0, True)
+        self.advance_time_and_run(secs_since_plunge)  # plunger timeout is 3s
+
+    def test_single_player_game_start(self):
+        self._start_single_player_game(4)
+
+        # 4 secs since plunge means ball is on the pf
         self.assertEqual(1, self.machine.playfield.balls)
+        self.assertEqual(self.machine.ball_devices.bd_drain.balls, 0)
+        self.assertEqual(self.machine.ball_devices.bd_trough.balls, 3)
+        self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 0)
+
+    def test_ball_fails_to_plunge(self):
+        self._start_single_player_game(1)
+
+        # ball lands back on the plunger
+        self.hit_switch_and_run('s_plungerlane', 1)
+
+        self.assertEqual(0, self.machine.playfield.balls)
+        self.assertEqual(self.machine.ball_devices.bd_drain.balls, 0)
+        self.assertEqual(self.machine.ball_devices.bd_trough.balls, 3)
+
+        # ball is not added back to the plunger count. Need to figure out a
+        # test for this.
+        # self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 1)
+
+        # player tries to eject again and fails
+        self.release_switch_and_run('s_plungerlane', 2)
+        self.hit_switch_and_run('s_plungerlane', 1)
+        self.assertEqual(0, self.machine.playfield.balls)
+
+        # finally they get it
+        self.release_switch_and_run('s_plungerlane', 3)
+        self.assertEqual(1, self.machine.playfield.balls)
+
+    def test_ball_launch_from_button(self):
+        self.hit_and_release_switch('s_start')
+        self.advance_time_and_run()
+        self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 1)
+
+        self.hit_and_release_switch('s_launch')
+        self.advance_time_and_run(4)  # plunger lane timeout is 3 secs
+
+        self.assertFalse(self.machine.switch_controller.is_active(
+            's_plungerlane'))
+
+        self.assertEqual(self.machine.ball_devices.bd_plunger.balls, 0)
+        self.assertEqual(1, self.machine.playfield.balls)
+
+
+# class TestBndFull(FullMachineTestCase):
+#
+#     def test_integrated_attract_mode(self):
+#         pass
