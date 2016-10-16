26ea5310402e1c6a11abdd11b5477bdb7c447f39
Brian Madden <brian@missionpinball.com>
2016-10-15 20:25:58 -0500

    jukebox mode initial timer fix

--- tests/test_bnd.py
+++ tests/test_bnd.py
@@ -151,7 +151,7 @@ class TestBnd(MpfMachineTestCase):
 
         self.assertModeNotRunning('world_tour')
 
-        # # shot the lower vuk to start the mode
+        # shoot the lower vuk to start the mode
         self.hit_switch_and_run('s_lowervukopto', 1)
 
         self.assertEqual(self.machine.achievements.world_tour.state, 'started')
@@ -301,111 +301,41 @@ class TestBnd(MpfMachineTestCase):
             self.assertLedColors('l_world_tour', ['red'], 1)
             self.assertEqual(self.machine.achievements.world_tour.state, 'completed')
 
-
-    def test_mission_rotator(self):
+    def test_jukebox_mode(self):
         self._start_single_player_game(1)
+        self.assertEqual(self.machine.achievements.jukebox.state, 'enabled')
 
-        # none of the mission lights should be lit
-        self.assertEqual([0, 0, 0],
-            self.machine.leds.l_world_tour.hw_driver.current_color)
-        self.assertEqual([0, 0, 0],
-            self.machine.leds.l_money_bags.hw_driver.current_color)
-        self.assertEqual([0, 0, 0],
-            self.machine.leds.l_music_awards.hw_driver.current_color)
-        self.assertEqual([0, 0, 0],
-            self.machine.leds.l_jukebox_insert.hw_driver.current_color)
-        self.assertEqual([0, 0, 0],
-            self.machine.leds.l_play_poker.hw_driver.current_color)
-
-        # mission rotator should be active
-        self.assertTrue(self.machine.modes.mission_rotator.active)
-
-        # mission select should not be active
-        self.assertTrue(self.machine.shots.light_mission_select.enabled)
-        self.assertEqual('flash',
-            self.machine.shots.light_mission_select.profiles[0]['running_show'].name)
-
-        # hit the shot to enable the mission rotator
+        # shoot the right ramp
         self.hit_and_release_switch('s_rightrampopto')
 
-        # mission rotator should be active
-        self.assertTrue(self.machine.modes.mission_rotator.active)
+        # # hit the sling until world tour is lit
+        x = 0
+        while self.machine.achievements.jukebox.state != 'selected':
 
-        # one of the mission shots should randomly be selected and flashing,
-        # the others should be off
+            if x > 5:
+                raise AssertionError("Mode failed to be selected")
 
-        temp_list = list()
-
-        temp_list.append(self.machine.shots.world_tour.profiles[0]['running_show'].name)
-        temp_list.append(self.machine.shots.money_bags.profiles[0]['running_show'].name)
-        temp_list.append(self.machine.shots.music_awards.profiles[0]['running_show'].name)
-        temp_list.append(self.machine.shots.jukebox_mode.profiles[0]['running_show'].name)
-        temp_list.append(self.machine.shots.play_poker.profiles[0]['running_show'].name)
-
-        self.assertEqual(1, len([x for x in temp_list if x == 'flash']))
-        self.assertEqual(4, len([x for x in temp_list if x == 'off']))
-
-        # move the flashing one to world tour
-        while (self.machine.shots.world_tour.profiles[0]['running_show'].name !=
-                'flash'):
-
+            x += 1
             self.hit_and_release_switch('s_rightsling')
+            self.advance_time_and_run()
 
-        # start the world_tour mission
-        self.machine.switch_controller.process_switch('s_lowervukopto', logical=True)
-        self.advance_time_and_run()
-        self.assertTrue(self.machine.modes.world_tour.active)
+        self.assertModeNotRunning('jukebox_mode')
 
-        # wait for it to time out
-        self.advance_time_and_run(30)
-        self.assertFalse(self.machine.modes.world_tour.active)
+        # shoot the lower vuk to start the mode
+        self.hit_switch_and_run('s_lowervukopto', 1)
 
-        # missions should be all off now
-        self.assertEqual([0, 0, 0],
-            self.machine.leds.l_world_tour.hw_driver.current_color)
-        self.assertEqual([0, 0, 0],
-            self.machine.leds.l_money_bags.hw_driver.current_color)
-        self.assertEqual([0, 0, 0],
-            self.machine.leds.l_music_awards.hw_driver.current_color)
-        self.assertEqual([0, 0, 0],
-            self.machine.leds.l_jukebox_insert.hw_driver.current_color)
-        self.assertEqual([0, 0, 0],
-            self.machine.leds.l_play_poker.hw_driver.current_color)
+        self.assertEqual(self.machine.achievements.jukebox.state, 'started')
+        self.assertEqual(self.get_timer('jb_intro_timer').ticks_remaining, 2)
+        self.advance_time_and_run(1)
+        self.assertEqual(self.get_timer('jb_intro_timer').ticks_remaining, 1)
 
-        # mission select should be active
-        self.assertTrue(self.machine.modes.light_mission_select.active)
+        # advance enough time for it to kick the ball out
+        self.advance_time_and_run(2)
 
-        # l_ramp_fire should be flashing
-        temp_list = list()
-        temp_list.append(self.machine.leds.l_ramp_fire.hw_driver.current_color)
-        self.advance_time_and_run(.333)
-        temp_list.append(self.machine.leds.l_ramp_fire.hw_driver.current_color)
-        self.assertIn([0,0,0], temp_list)
-        self.assertIn([255,255,255], temp_list)
+        self.assertModeRunning('jukebox_mode')
 
-        # mission rotator should not be active
-        self.assertFalse(self.machine.modes.mission_rotator.active)
-
-        # hit the shot to enable the mission rotator
-        self.hit_and_release_switch('s_rightrampopto')
-
-        # mission rotator should be active
-        self.assertTrue(self.machine.modes.mission_rotator.active)
-
-        # lit mission should have rotated
-        self.assertEqual('off',
-            self.machine.shots.world_tour.profiles[0]['running_show'].name)
-        self.assertEqual('flash',
-            self.machine.shots.money_bags.profiles[0]['running_show'].name)
-        self.assertEqual('off',
-            self.machine.shots.music_awards.profiles[0]['running_show'].name)
-        self.assertEqual('off',
-            self.machine.shots.jukebox_mode.profiles[0]['running_show'].name)
-        self.assertEqual('off',
-            self.machine.shots.play_poker.profiles[0]['running_show'].name)
-
-
-# class TestBndFull(FullMachineTestCase):
-#
-#     def test_integrated_attract_mode(self):
-#         pass
+        self.assertEqual(self.get_timer('jukebox_mode_timer').ticks_remaining, 10)
+        self.advance_time_and_run(1.25)
+        self.assertEqual(self.get_timer('jukebox_mode_timer').ticks_remaining, 9)
+        self.advance_time_and_run(1.25)
+        self.assertEqual(self.get_timer('jukebox_mode_timer').ticks_remaining, 8)
