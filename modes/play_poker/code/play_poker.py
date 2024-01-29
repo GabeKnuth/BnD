c7156843f37a67426eb791cd25480c527a60c02d
Gabe Knuth <gabe@gabeknuth.com>
2024-01-29 12:02:48 -0600

    laptop catchup

--- modes/play_poker/code/play_poker.py
+++ modes/play_poker/code/play_poker.py
@@ -61,7 +61,8 @@ class PlayPoker(Mode):
 
         self.player.poker_cards.append(self.player.poker_current_card)
 
-        self.machine.events.post('poker_card_locked')
+        if len(self.player.poker_cards) != 5:
+            self.machine.events.post('poker_card_locked')
 
         self.machine.events.post(
             'poker_card_{}_solid'.format(len(self.player.poker_cards)),
